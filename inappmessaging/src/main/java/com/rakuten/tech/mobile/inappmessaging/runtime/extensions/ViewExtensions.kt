9ae430e6d2dfd20d6721ffc35189054f7c41f1a4
mau <maureenorea.clores@rakuten.com>
2024-02-15 11:02:23 +0900

    gora tooltip bottom buttons issue

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/extensions/ViewExtensions.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/extensions/ViewExtensions.kt
@@ -1,20 +1,30 @@
 package com.rakuten.tech.mobile.inappmessaging.runtime.extensions
 
+import android.content.Context
 import android.content.res.Resources
 import android.graphics.Rect
+import android.util.DisplayMetrics
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 
 internal fun View.isVisible(outPosition: Rect? = null): Boolean {
     if (!isShown) {
         return false
     }
 
+//    val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+//    val displayMetrics = DisplayMetrics()
+//    wm.defaultDisplay.getRealMetrics(displayMetrics)
+
     val actualPosition = outPosition ?: Rect()
-    val screen = Rect(
-        0, 0,
-        Resources.getSystem().displayMetrics.widthPixels, Resources.getSystem().displayMetrics.heightPixels,
-    )
+//    val screen = Rect(
+//        0, 0,
+//        Resources.getSystem().displayMetrics.widthPixels, Resources.getSystem().displayMetrics.heightPixels,
+//    )
+    val windowVisibleFrame = Rect()
+    getWindowVisibleDisplayFrame(windowVisibleFrame)
+    val screen = Rect(0, 0, windowVisibleFrame.right, windowVisibleFrame.bottom)
 
     return getGlobalVisibleRect(actualPosition) && Rect.intersects(actualPosition, screen)
 }
